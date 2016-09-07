63301897102064bfc6ea088bdfdac5b5b8664e6b
Jordi Llopis <jllopisol@gmail.com>
2016-09-07 16:43:23 +0200

    First Release
    beta1.0

---
+++ ftpvitainstaller-c-sharp/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ftpvitainstaller_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.gameNameLabel = new System.Windows.Forms.Label();
+            this.GameGroupBox = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fileSizeLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ipBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.GameGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Game...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gameNameLabel
+            // 
+            this.gameNameLabel.AutoSize = true;
+            this.gameNameLabel.Location = new System.Drawing.Point(124, 32);
+            this.gameNameLabel.Name = "gameNameLabel";
+            this.gameNameLabel.Size = new System.Drawing.Size(33, 13);
+            this.gameNameLabel.TabIndex = 1;
+            this.gameNameLabel.Text = "None";
+            // 
+            // GameGroupBox
+            // 
+            this.GameGroupBox.Controls.Add(this.pictureBox1);
+            this.GameGroupBox.Controls.Add(this.fileSizeLabel);
+            this.GameGroupBox.Controls.Add(this.gameNameLabel);
+            this.GameGroupBox.Controls.Add(this.button1);
+            this.GameGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.GameGroupBox.Name = "GameGroupBox";
+            this.GameGroupBox.Size = new System.Drawing.Size(304, 237);
+            this.GameGroupBox.TabIndex = 2;
+            this.GameGroupBox.TabStop = false;
+            this.GameGroupBox.Text = "Game";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fileSizeLabel
+            // 
+            this.fileSizeLabel.AutoSize = true;
+            this.fileSizeLabel.Location = new System.Drawing.Point(14, 57);
+            this.fileSizeLabel.Name = "fileSizeLabel";
+            this.fileSizeLabel.Size = new System.Drawing.Size(52, 13);
+            this.fileSizeLabel.TabIndex = 2;
+            this.fileSizeLabel.Text = "File Size: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.statusLabel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.portBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ipBox);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 256);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 195);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(285, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Install";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(6, 101);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(285, 35);
+            this.statusLabel.TabIndex = 5;
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ":";
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(236, 32);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(55, 20);
+            this.portBox.TabIndex = 3;
+            this.portBox.Text = "1337";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PSVita IP:";
+            // 
+            // ipBox
+            // 
+            this.ipBox.Location = new System.Drawing.Point(71, 32);
+            this.ipBox.Name = "ipBox";
+            this.ipBox.Size = new System.Drawing.Size(143, 20);
+            this.ipBox.TabIndex = 1;
+            this.ipBox.Text = "192.168.1.25";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 139);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(285, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Value = 50;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 464);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.GameGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PSVita FTP Game Installer";
+            this.GameGroupBox.ResumeLayout(false);
+            this.GameGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label gameNameLabel;
+        private System.Windows.Forms.GroupBox GameGroupBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ipBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label fileSizeLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ftpvitainstaller-c-sharp/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using SharpCompress.Common;
+using SharpCompress.Reader;
+using SharpCompress.Archive;
+using SharpCompress.Archive.Zip;
+using WinSCP;
+using PeXploit;
+using System.IO;
+
+
+namespace ftpvitainstaller_c_sharp
+{
+    public partial class Form1 : Form
+    {
+
+        private PARAM_SFO param;
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Value = 0;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"C:/temp"))
+            {
+                Console.Write("Temp directory deleted");
+                Directory.Delete(@"C:/temp", true);
+            }
+            openFileDialog1.Title = "Open Game...";
+            openFileDialog1.Filter = "VPK Files |*.vpk|Zip Files |*.zip";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                gameNameLabel.Text = openFileDialog1.FileName;
+                bool isvpk = false;
+                if (openFileDialog1.FileName.Contains(".vpk"))
+                {
+                    isvpk = true;
+                    File.Move(openFileDialog1.FileName, Path.ChangeExtension(openFileDialog1.FileName, ".zip"));
+                    openFileDialog1.FileName = Path.ChangeExtension(openFileDialog1.FileName, ".zip");
+                }
+                var archive = ArchiveFactory.Open(openFileDialog1.FileName);
+                fileSizeLabel.Text = "File Size: " + archive.TotalSize + " bytes";
+                statusLabel.Text = "Reading File...";
+                foreach (var file_ in archive.Entries)
+                {
+
+                    if (file_.Key.Contains("sce_sys") || file_.Key == "eboot.bin")
+                    {
+                        file_.WriteToDirectory(@"C:\temp\install", ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                    }
+                    else
+                    {
+                        file_.WriteToDirectory(@"C:\temp\data", ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                    }
+
+                }
+
+                archive.Dispose();
+                
+                pictureBox1.ImageLocation = @"C:\temp\install\sce_sys\pic0.png";
+                statusLabel.Text = "Reading SFO...";
+
+                param = new PARAM_SFO(@"C:\temp\install\sce_sys\param.sfo");
+
+                gameNameLabel.Text = param.Title;
+
+                statusLabel.Text = "Game Loaded";
+                button2.Enabled = true;
+
+                if (isvpk)
+                {                    
+                    File.Move(openFileDialog1.FileName, Path.ChangeExtension(openFileDialog1.FileName, ".vpk"));
+                }
+            }            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Install();
+
+        }
+
+        private void Install()
+        {
+            // CREATE VPK
+            this.button2.Enabled = false;
+            statusLabel.Text = @"Creating .vpk...";
+
+            using (var archive = ZipArchive.Create())
+            {
+                archive.AddAllFromDirectory(@"C:\temp\install");
+                archive.SaveTo(@"C:\\temp\\installer.vpk", CompressionType.None);
+            }
+
+            statusLabel.Text = @"VPK Created";
+            progressBar1.Value = 25;
+
+            System.Threading.Thread.Sleep(1000);
+
+            // TRANSFER VPK
+
+            statusLabel.Text = "Connecting...";
+            try
+            {
+                // Setup session options
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Ftp,
+                    HostName = ipBox.Text,
+                    PortNumber = Convert.ToInt32(portBox.Text),
+                    UserName = "anonymous",
+                    Password = ""
+                };
+
+                using (Session session = new Session())
+                {
+                    // Connect
+                    session.Open(sessionOptions);
+                    statusLabel.Text = "Connected";
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+
+                    TransferOperationResult transferResult;
+                    statusLabel.Text = "Copying installer...";
+                    transferResult = session.PutFiles(@"c:\temp\installer.vpk", "ux0:/VPKs/", false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Print results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+
+                    }
+                    statusLabel.Text = "Install the VPK at ux0:/VPKs/";
+                    progressBar1.Value = 50;
+                    DialogResult result;
+                    result = MessageBox.Show("Please go to ux0:/VPKs on the PSVita and install the installer.vpk file, then click OK. \n\nATTENTION! ONLY press OK when the installation is complete.", "Installer", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                    if (result == System.Windows.Forms.DialogResult.OK)
+                    {
+                        // Transfer data files
+                        statusLabel.Text = "Installing, please wait...";
+                        progressBar1.Value = 75;
+                        transferResult = session.PutFiles(@"c:\temp\data\*", "ux0:/app/" + param.TitleID + "/", false, transferOptions);
+
+                        // Throw on any error
+                        transferResult.Check();
+
+                        // Print results
+                        foreach (TransferEventArgs transfer in transferResult.Transfers)
+                        {
+                            Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+                            
+                            
+                        }
+                        MessageBox.Show(param.Title + " installed successfully.", "Congrats!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        progressBar1.Value = 100;
+                        statusLabel.Text = "Completed!";
+                        button2.Enabled = true;
+
+                        // Clean temp directory
+                        Directory.Delete(@"C:/temp", true);
+                    }
+                    else
+                    {
+                        button2.Enabled = true;
+                        statusLabel.Text = "Installation aborted.";
+                        return;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex);
+                statusLabel.Text = "CONNECTION ERROR";
+                progressBar1.Value = 0;
+                button2.Enabled = true;
+            }
+        }
+
+    }
+}
+
